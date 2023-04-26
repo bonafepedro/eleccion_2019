a46e9a643ffd999e9c01cedc7baa52a8a0d6a4d3
Josías Gómez <82390052+JosiasGomez@users.noreply.github.com>
2023-04-26 11:30:54 -0300

    Update tribunal_electoral_scrape.py
    se agregaron circuitos

--- code/tribunal_electoral_scrape.py
+++ code/tribunal_electoral_scrape.py
@@ -3,11 +3,12 @@
 
 
 
-import requests 
 from bs4 import BeautifulSoup as bs
 import itertools
 import string
 import pandas as pd
+import asyncio
+import httpx
 
 cargos = ['CA2','CA3','CA4','CA5','CA7','CA8']
 
@@ -33,90 +34,80 @@ headers = {
 }
 
 def circuitos():
-    num_1 = [i for i in range(1,10)]
-    num_2 = [i for i in range(10,15)]
     
+    #Para hacer las consultas a la API hay que definir los cargos y los circuitos. Los cargos fueron definidos en una variable
+    #global más arriba. En el caso de los circuitos se presentan diferentes casos que condicionan el request.
+    #Se detecto que existen seis casos. La de número que van respectivamente de 1-9, 10-99 y 99-400, por un lado. Por el otro,
+    #se suma la presencia de una letra para cada caso anteriormente definido. Por ello quedan conformados seis casos posibles.
+    
+    
+    #Se crea una lista con letras
     lett = list(string.ascii_uppercase)[0:17]
     
-    casos_1 = list(itertools.product(num_1,lett))
-    casos_2 = list(itertools.product(num_2,lett))
-    
+    #Se definen las listas con los tres casos numéricos
     circuito_numeros_1 = [i for i in range(1,10)]
-    circuito_numeros_2 = [i for i in range(10,15)]
+    circuito_numeros_2 = [i for i in range(10,100)]
+    circuitos_numeros_3 = [i for i in range(100,401)]
     
-    circuito_numero_y_letras_1 = []
+    #Luego se generan los casos de número + letra
+    casos_1 = list(itertools.product(circuito_numeros_1,lett))
+    casos_2 = list(itertools.product(circuito_numeros_2,lett))
+    casos_3 = list(itertools.product(circuitos_numeros_3,lett))
     
-    for caso in casos_1:
-        circuito_numero_y_letras_1.append(str(caso[0])+caso[1])
+    #Finalmente se crean las listas con número + letra. El paso anterior podría haberse evitado, pero a fines de legibilidad se 
+    #obtó por separalos
+    circuito_numero_y_letras_1 = [str(caso[0])+caso[1] for caso in casos_1]
+    circuito_numero_y_letras_2 = [str(caso[0])+caso[1] for caso in casos_2]
+    circuito_numero_y_letras_3 = [str(caso[0])+caso[1] for caso in casos_3]
     
-    circuito_numero_y_letras_2 = []
+    #Se generan los iteradores que combinan circuitos + cargos
+    iter_1 = list(itertools.product(circuito_numeros_1,cargos))
+    iter_2 = list(itertools.product(circuito_numero_y_letras_1,cargos))
+    iter_3 = list(itertools.product(circuito_numeros_2,cargos))
+    iter_4 = list(itertools.product(circuito_numero_y_letras_2,cargos))
+    iter_5 = list(itertools.product(circuitos_numeros_3,cargos))
+    iter_6 = list(itertools.product(circuito_numero_y_letras_3,cargos))
     
-    for caso in casos_2:
-        circuito_numero_y_letras_2.append(str(caso[0])+caso[1])
-        
-    return circuito_numeros_1,circuito_numero_y_letras_1,circuito_numeros_2,circuito_numero_y_letras_2
+    #Finalmente se definen los diferentes paquetes de links que harán las requests necesarias considerando cada caso
+    links_1 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20%20_{i[1]}_0.htm' for i in iter_1]
+    links_2 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20_{i[1]}_0.htm' for i in iter_2]
+    links_3 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20_{i[1]}_0.htm' for i in iter_3]
+    links_4 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20_{i[1]}_0.htm' for i in iter_4]
+    links_5 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20_{i[1]}_0.htm' for i in iter_5]
+    links_6 = [f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20_{i[1]}_0.htm' for i in iter_6]
+    
+    
+    
+    return list(itertools.chain(links_1,links_2,links_3,links_4,links_5))
 
 def get_line(response):
     return [i.text for i in bs(response.text).select('tr td') if i.text != '']
 
-def get_data():
+#Considerando que se realizarán unas ~7k requests (400+400*16), se decide definir la función get_data como una
+#función asíncrona, es decir, que cuando por x razón la request se demore se ejecutará otra en el tiempo que la pimera
+#está "cargando"
+
+async def get_data():
+    
+    #se define un timeout mayor que el default para evitar que se rompa el código
+    timeout = httpx.Timeout(30.0)
     
     datos = []
     
-    caso_1, caso_2, caso_3, caso_4 = circuitos()
+    #se toman los casos de la función circuitos
+    links = circuitos()
     
-    iter_1 = list(itertools.product(caso_1,cargos))
-    iter_2 = list(itertools.product(caso_2,cargos))
-    iter_3 = list(itertools.product(caso_3,cargos))
-    iter_4 = list(itertools.product(caso_4,cargos))
-    
-    
-    for i in iter_1:
-        try: 
-            response = requests.get(f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20%20_{i[1]}_0.htm',
-                                        cookies=cookies,
-                                        headers=headers,)
-            response.raise_for_status()
-            dato = get_line(response)
-            datos.append(dato)
-        except:
-            next
-    
-    for i in iter_2:
-        try: 
-            response = requests.get(f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20_{i[1]}_0.htm',
-                                        cookies=cookies,
-                                        headers=headers,)
-            response.raise_for_status()
-            dato = get_line(response)
-            datos.append(dato)
-        except:
-            next
+    #se define un cliente async y se crea una lista de tareas por cada link provisto
+    async with httpx.AsyncClient() as client:
 
-    for i in iter_3:
-        try: 
-            response = requests.get(f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20%20_{i[1]}_0.htm',
-                                        cookies=cookies,
-                                        headers=headers,)
-            response.raise_for_status()
+        task = [client.get(link,timeout=timeout) for link in links]
+        for response_future in asyncio.as_completed(task):
+            response = await response_future
             dato = get_line(response)
             datos.append(dato)
-        except:
-            next
-    
-    for i in iter_4:
-        try: 
-            response = requests.get(f'https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20190512/Resultados/E20190512_C{i[0]}%20%20_{i[1]}_0.htm',
-                                        cookies=cookies,
-                                        headers=headers,)
-            response.raise_for_status()
-            dato = get_line(response)
-            datos.append(dato)
-        except:
-            next
-
 
     return datos
+
 
 
 # recibe como input el resultado de la función get_data()
